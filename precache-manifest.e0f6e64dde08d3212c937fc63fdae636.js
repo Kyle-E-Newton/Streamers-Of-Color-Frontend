d4e7faa9b84cfb445bee2efcbe1f9a87947f22a6
Kyle <kyle.e.newton@wsu.edu>
2020-06-08 20:03:55 -0700

    Updates

---
+++ precache-manifest.e0f6e64dde08d3212c937fc63fdae636.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7feafec6e81ecf83b3a2650d7773f1",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4e6c876e36e87ed553e9",
+    "url": "/website/static/css/main.48c35b37.chunk.css"
+  },
+  {
+    "revision": "c5d4a5816832845c00ee",
+    "url": "/website/static/js/13.c7a2f503.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/13.c7a2f503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7988ecc17d12a545f6b",
+    "url": "/website/static/js/14.083350ab.chunk.js"
+  },
+  {
+    "revision": "4e6c876e36e87ed553e9",
+    "url": "/website/static/js/main.95d37d14.chunk.js"
+  },
+  {
+    "revision": "b5ac23f5b13c99856eb8",
+    "url": "/website/static/js/reactPlayerDailyMotion.7ec036ff.chunk.js"
+  },
+  {
+    "revision": "fed1a288180e66744bb8",
+    "url": "/website/static/js/reactPlayerFacebook.e430a26b.chunk.js"
+  },
+  {
+    "revision": "8dd14d605e5faa6126d6",
+    "url": "/website/static/js/reactPlayerFilePlayer.9a0a0498.chunk.js"
+  },
+  {
+    "revision": "ce3c604aeb5e444abdf3",
+    "url": "/website/static/js/reactPlayerMixcloud.6ffeac43.chunk.js"
+  },
+  {
+    "revision": "dfd759a1b03888c63f1d",
+    "url": "/website/static/js/reactPlayerSoundCloud.d633c3e1.chunk.js"
+  },
+  {
+    "revision": "cc333fd8a56593e81510",
+    "url": "/website/static/js/reactPlayerStreamable.65c2cdb5.chunk.js"
+  },
+  {
+    "revision": "99604062b283ead6b131",
+    "url": "/website/static/js/reactPlayerTwitch.9720f6b8.chunk.js"
+  },
+  {
+    "revision": "0d9a50641065392b7da0",
+    "url": "/website/static/js/reactPlayerVidyard.9ac6a064.chunk.js"
+  },
+  {
+    "revision": "7585a36032d99827e4e1",
+    "url": "/website/static/js/reactPlayerVimeo.54902389.chunk.js"
+  },
+  {
+    "revision": "ed9f837728a0b3704807",
+    "url": "/website/static/js/reactPlayerWistia.236ef5c9.chunk.js"
+  },
+  {
+    "revision": "1d29c8cbab2eddf4aeca",
+    "url": "/website/static/js/reactPlayerYouTube.aacba187.chunk.js"
+  },
+  {
+    "revision": "e17a784339c3b9972a54",
+    "url": "/website/static/js/runtime-main.6d46ae2d.js"
+  },
+  {
+    "revision": "f5320c03ec7328d22a25090a4e098775",
+    "url": "/website/static/media/POCLogo.f5320c03.png"
+  }
+]);
