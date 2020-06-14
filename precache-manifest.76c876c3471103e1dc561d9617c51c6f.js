3c07f52810a63d7a88e4acbd25bc7b70e6994bba
Kyle <kyle.e.newton@wsu.edu>
2020-06-14 16:07:59 -0700

    Updates

---
+++ precache-manifest.76c876c3471103e1dc561d9617c51c6f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a56e828aa0051e3a54f813a9c6c0059",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22192347761c961c5e9c",
+    "url": "./static/css/main.48c35b37.chunk.css"
+  },
+  {
+    "revision": "440d7e373026e4d86a2c",
+    "url": "./static/js/13.773e2610.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/13.773e2610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7988ecc17d12a545f6b",
+    "url": "./static/js/14.083350ab.chunk.js"
+  },
+  {
+    "revision": "22192347761c961c5e9c",
+    "url": "./static/js/main.8d444f03.chunk.js"
+  },
+  {
+    "revision": "b5ac23f5b13c99856eb8",
+    "url": "./static/js/reactPlayerDailyMotion.7ec036ff.chunk.js"
+  },
+  {
+    "revision": "fed1a288180e66744bb8",
+    "url": "./static/js/reactPlayerFacebook.e430a26b.chunk.js"
+  },
+  {
+    "revision": "8dd14d605e5faa6126d6",
+    "url": "./static/js/reactPlayerFilePlayer.9a0a0498.chunk.js"
+  },
+  {
+    "revision": "ce3c604aeb5e444abdf3",
+    "url": "./static/js/reactPlayerMixcloud.6ffeac43.chunk.js"
+  },
+  {
+    "revision": "dfd759a1b03888c63f1d",
+    "url": "./static/js/reactPlayerSoundCloud.d633c3e1.chunk.js"
+  },
+  {
+    "revision": "cc333fd8a56593e81510",
+    "url": "./static/js/reactPlayerStreamable.65c2cdb5.chunk.js"
+  },
+  {
+    "revision": "99604062b283ead6b131",
+    "url": "./static/js/reactPlayerTwitch.9720f6b8.chunk.js"
+  },
+  {
+    "revision": "0d9a50641065392b7da0",
+    "url": "./static/js/reactPlayerVidyard.9ac6a064.chunk.js"
+  },
+  {
+    "revision": "7585a36032d99827e4e1",
+    "url": "./static/js/reactPlayerVimeo.54902389.chunk.js"
+  },
+  {
+    "revision": "ed9f837728a0b3704807",
+    "url": "./static/js/reactPlayerWistia.236ef5c9.chunk.js"
+  },
+  {
+    "revision": "1d29c8cbab2eddf4aeca",
+    "url": "./static/js/reactPlayerYouTube.aacba187.chunk.js"
+  },
+  {
+    "revision": "ec33b0dfdabb694a3cfb",
+    "url": "./static/js/runtime-main.70f4afe6.js"
+  },
+  {
+    "revision": "f5320c03ec7328d22a25090a4e098775",
+    "url": "./static/media/POCLogo.f5320c03.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Streamers-Of-Color-Frontend/precache-manifest.b17d0f3b64b1239e4c3d136da1337323.js"
+  "./precache-manifest.76c876c3471103e1dc561d9617c51c6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Streamers-Of-Color-Frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
